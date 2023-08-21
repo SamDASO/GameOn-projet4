9a734149b89e078afeb1bf05c734f5d4e36cddd6
Samanta DA SILVA <developpement.sam@gmail.com>
2023-08-21 14:03:27 +0200

    Issue 2,  à tester

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -32,10 +32,18 @@ function closeModal() {
 // Validation when the user submit the form
 function validate() {
 
+  //Stored value - prevent deleting the users value if the form is not valid when sbmit
+  let storedfirst = firstInput.value;
+  let storedlast = lastInput.value;
+  let storedemail = emailInput.value;
+  let storedcompetition = competitionInput.value;
+
   //If the first name is not at least 2 caracters
   let firstInput = document.getElementById('first');
 
+
   if (firstInput.value.trim().length < 2) {
+    firstInput.value = storedfirst;
     return false;
   }
 
@@ -43,6 +51,7 @@ function validate() {
   let lastInput = document.getElementById('last');
 
   if (lastInput.value.trim().length < 2) {
+    lastInput.value = storedlast;
     return false;
   }
 
@@ -51,6 +60,15 @@ function validate() {
   let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   if (!validRegex.test(emailInput.value)) {
+    emailInput.value = storedemail;
+    return false;
+  }
+
+  //If the quantity in the number of competition field is not a number
+  let competitionInput = document.getElementById('quantity');
+
+  if (isNan(competitionInput.value)) {
+    competitionInput.value = storedcompetition;
     return false;
   }
 
