51c6e2cc1fd9c61f690dba75895ac8a64b0a8673
Samanta DA SILVA <developpement.sam@gmail.com>
2023-08-31 23:04:31 +0200

    Issues 2 et 3 traitées. Validation du formulaire avant envoi et message d'erreurs fonctionnels

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,7 +16,10 @@ const modalClose = document.querySelectorAll(".close");
 let firstInput = document.getElementById('first');
 let lastInput = document.getElementById('last');
 let emailInput = document.getElementById('email');
+let dateInput = document.getElementById('birthdate');
 let competitionInput = document.getElementById('quantity');
+let locationInput = document.querySelectorAll('input[name="location"]');
+let termsInput = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -46,18 +49,21 @@ function validate(submit) {
   let storedfirst = firstInput.value;
   let storedlast = lastInput.value;
   let storedemail = emailInput.value;
+  let storeddate = dateInput.value;
   let storedcompetition = competitionInput.value;
-
+  let storedlocation = locationInput.value;
 
   let formValid = 0;
 
-  formValid = firstValidation() + lastValidation() + emailValidation() + competitionValidation();
+  formValid = firstValidation() + lastValidation() + emailValidation() + dateValidation() + competitionValidation() + locationValidation() + termsValidation();
 
   if (formValid > 0) {
     lastInput.value = storedlast;
     firstInput.value = storedfirst;
     emailInput.value = storedemail;
     competitionInput.value = storedcompetition;
+    dateInput.value = storeddate;
+    locationInput.value = storedlocation;
     return false;
 
   } else {
@@ -106,7 +112,7 @@ function emailValidation() {
 
   if (!validRegex.test(emailInput.value)) {
     emailInput.parentElement.setAttribute('data-error-visible', 'true');
-    emailInput.parentElement.setAttribute('data-error', "L'e-mail entré n'est pas valide.");
+    emailInput.parentElement.setAttribute('data-error', "L'e-mail entré n'est pas valide");
     return 1;
 
   } else {
@@ -116,14 +122,30 @@ function emailValidation() {
   }
 }
 
+//If the birthdate is not registred
+
+function dateValidation() {
+
+  if (!dateInput.value) {
+    dateInput.parentElement.setAttribute('data-error-visible', 'true');
+    dateInput.parentElement.setAttribute('data-error', "Vous devez entrer votre date de naissance");
+    return 1;
+
+  } else {
+    dateInput.parentElement.setAttribute('data-error-visible', 'false');
+    dateInput.parentElement.removeAttribute('data-error');
+    return 0;
+  }
+}
+
 
 //If the quantity in the number of competition field is not a number
 
 function competitionValidation() {
 
-  if (isNaN(competitionInput.value)) {
+  if (isNaN(competitionInput.value) || (!competitionInput.value)) {
     competitionInput.parentElement.setAttribute('data-error-visible', 'true');
-    competitionInput.parentElement.setAttribute('data-error', "Ce champ ne peut contenir que des chiffres.");
+    competitionInput.parentElement.setAttribute('data-error', "Ce champ est obligatoire et ne peut contenir que des chiffres");
     return 1;
 
   } else {
@@ -134,5 +156,41 @@ function competitionValidation() {
   }
 }
 
+//If the location is set
+
+function locationValidation() {
+
+  let isChecked = Array.from(locationInput).some(input => input.checked);
+  let group = document.querySelector('.locationData');
+
+  if (!isChecked) {
+    group.setAttribute('data-error-visible', 'true');
+    group.setAttribute('data-error', "Vous devez choisir un tournoi auquel vous souhaitez participer");
+    return 1;
+
+  } else {
+    group.setAttribute('data-error-visible', 'false');
+    group.removeAttribute('data-error');
+
+    return 0;
+  }
+}
+
+//If the terms of use are not accept
+
+function termsValidation() {
+
+  if (!termsInput.checked) {
+    termsInput.parentElement.setAttribute('data-error-visible', 'true');
+    termsInput.parentElement.setAttribute('data-error', "Vous devez avoir lu et accepté les conditions d'utilisation pour pouvoir vous inscrire");
+    return 1;
+
+  } else {
+    termsInput.parentElement.setAttribute('data-error-visible', 'false');
+    termsInput.parentElement.removeAttribute('data-error');
+
+    return 0;
+  }
+}
 
 
